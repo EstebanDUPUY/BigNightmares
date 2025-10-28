8359ce2e20a0a9f9dd5b6cd4de98891422a8511f
Sylvie HOAREAU <hoareausms@gmail.com>
2025-10-28 15:34:48 +0400

    feat: ajout des scripts pour le menu principal

---
+++ Assets/Scripts/SylvieScripts/S_MenuParallax.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MenuParallax : MonoBehaviour
+{
+    public float offsetMultiplier = 1f;
+    public float smoothTime = 3f;
+
+    private Vector2 startPosition;
+    private Vector3 veolocity;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
