e1c37ff9734183d11c36bb16c81a95fd328a21a0
Sylvie HOAREAU <hoareausms@gmail.com>
2025-10-29 14:22:43 +0400

    fix: correction AudioManager Credits

--- Assets/Scripts/SylvieScripts/S_Credits.cs
+++ Assets/Scripts/SylvieScripts/S_Credits.cs
@@ -5,7 +5,7 @@ public class S_Credits : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-         S_MusicManager.Instance.PlayMusic("MainMenu");
+         S_MusicManager.Instance.PlayMusic("Credits");
     }
 
 }
