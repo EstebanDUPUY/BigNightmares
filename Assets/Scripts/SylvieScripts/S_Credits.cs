770a142c1dfe7e2632ae7e18f7608f8324c9fd4e
Sylvie HOAREAU <hoareausms@gmail.com>
2025-10-31 12:50:52 +0400

    feat: mise à jour de la scène Credits

--- Assets/Scripts/SylvieScripts/S_Credits.cs
+++ Assets/Scripts/SylvieScripts/S_Credits.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class S_Credits : MonoBehaviour
 {
+    public float scrollSpeed = 400f;
+
+    public RectTransform rectTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-         S_MusicManager.Instance.PlayMusic("Credits");
+        S_MusicManager.Instance.PlayMusic("Credits");
+        rectTransform = GetComponent<RectTransform>();
+    }
+    
+    private void Update()
+    {
+        rectTransform.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
     }
 
 }
